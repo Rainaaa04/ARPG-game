869bf4f3c050061c81a5f6906db6e507f7c4db49
Rainaaa04 <141323987+Rainaaa04@users.noreply.github.com>
2023-09-15 12:12:17 +0530

    Initial commit

---
+++ collectables/sword.gd
@@ -0,0 +1,11 @@
+extends "res://collectables/collectable.gd"
+
+@onready var animations = $AnimationPlayer
+
+func collect(inventory: Inventory):
+	animations.play("spin")
+	await animations.animation_finished
+	super(inventory)
+	
+	
+	
---
+++ scenes/world.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+@onready var heartsContainer = $CanvasLayer/heartsContainer
+@onready var player = $TileMap/Player
+
+
+func _ready() -> void:
+	heartsContainer.setMaxHearts(player.maxHealth)
+	heartsContainer.updateHearts(player.currentHealth)
+	player.healthChanged.connect(heartsContainer.updateHearts)
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_inventory_gui_closed() -> void:
+	get_tree().paused = false
+
+
+func _on_inventory_gui_opened() -> void:
+	get_tree().paused = true
